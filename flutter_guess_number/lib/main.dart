c52c0890f95cea02acc4052a20b60649d3dc1cda
Jakub Plona <plonajakub@gmail.com>
2020-11-24 23:57:01 +0100

    [Flutter] Add settings page layout

--- flutter_guess_number/lib/main.dart
+++ flutter_guess_number/lib/main.dart
@@ -6,18 +6,29 @@ void main() {
   runApp(MyApp());
 }
 
+class CustomColors {
+  static const Color primaryColor = Color(0xFFFF8B80);
+  static const Color primaryColorDark = Color(0xFFC85B53);
+  static const Color accent = Color(0xFF80F4FF);
+  static const Color accentDark = Color(0xFF46C1CC);
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Guess Number',
       theme: ThemeData(
-        primaryColor: const Color(0xFFFF8B80),
-        primaryColorDark: const Color(0xFFC85B53),
-        accentColor: const Color(0xFF80F4FF),
-        backgroundColor: Colors.white,
-        brightness: Brightness.light
-      ),
+          primaryColor: CustomColors.primaryColor,
+          primaryColorDark: CustomColors.primaryColorDark,
+          accentColor: CustomColors.accent,
+          backgroundColor: Colors.white,
+          brightness: Brightness.light,
+          sliderTheme: SliderThemeData(
+            thumbColor: CustomColors.accentDark,
+            activeTrackColor: CustomColors.accent,
+            inactiveTrackColor: CustomColors.accent,
+          )),
       home: Scaffold(
         resizeToAvoidBottomInset: true,
         appBar: AppBar(
--- flutter_guess_number/lib/pages/settings-page.dart
+++ flutter_guess_number/lib/pages/settings-page.dart
@@ -1,16 +1,111 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatefulWidget {
+class SettingsPage extends StatelessWidget {
+  static const String pageTitle = 'Settings';
+
   @override
-  _SettingsPageState createState() => _SettingsPageState();
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.fromLTRB(8, 8, 0, 0),
+          child: Row(
+            children: [
+              Icon(Icons.keyboard_arrow_left),
+              Icon(Icons.videogame_asset)
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 60, 0, 60),
+          child: Text(
+            pageTitle,
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            children: [
+              SliderWithInfo(title: 'Maximum roll value', rightLimit: 100),
+              SizedBox(
+                height: 25,
+              ),
+              SliderWithInfo(title: 'Maximum guesses', rightLimit: 15),
+            ],
+          ),
+        )
+      ],
+    );
+  }
 }
 
-class _SettingsPageState extends State<SettingsPage> {
+class SliderWithInfo extends StatefulWidget {
+  static const String valueInfoText = 'Current value:';
+  static const int leftLimit = 1;
+  final String title;
+  final int rightLimit;
+
+  SliderWithInfo({@required this.title, @required this.rightLimit});
+
+  @override
+  _SliderWithInfoState createState() => _SliderWithInfoState();
+}
+
+class _SliderWithInfoState extends State<SliderWithInfo> {
+  double _currentSliderValue = 1;
+
+  _SliderWithInfoState() {
+    //load from shared prefs
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('SettingsPage'),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.title,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          Slider(
+            value: _currentSliderValue,
+            min: SliderWithInfo.leftLimit.toDouble(),
+            max: widget.rightLimit.toDouble(),
+            divisions: null,
+            label: null,
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderValue = value.roundToDouble();
+              });
+            },
+            onChangeEnd: (double value) {
+              // Save to shared prefs
+            },
+          ),
+          Row(
+            children: [
+              Text(
+                SliderWithInfo.valueInfoText,
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                child: Text(
+                  _currentSliderValue.toInt().toString(),
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
